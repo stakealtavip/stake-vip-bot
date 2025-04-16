ea4af977daa6da4abf20cbda391534dac66dfea6
stakealtavip <jeffindias.2019@gmail.com>
2025-04-16 12:19:35 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 
-import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-import requests
 import os
+import telebot
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,19 +14,19 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = InlineKeyboardMarkup()
+    markup = telebot.types.InlineKeyboardMarkup()
     markup.add(
-        InlineKeyboardButton("💳 Plano Mensal - R$50", callback_data="plano_50_Mensal"),
-        InlineKeyboardButton("💎 Plano Anual - R$300", callback_data="plano_300_Anual")
+        telebot.types.InlineKeyboardButton("💳 Plano Mensal - R$50", callback_data="plano_50_Mensal"),
+        telebot.types.InlineKeyboardButton("💎 Plano Anual - R$300", callback_data="plano_300_Anual")
     )
     texto = (
-        "✨💰 BEM-VINDO AO STAKE ALTA VIP 💰✨\n\n"
+        "✨👑 BEM-VINDO AO STAKE ALTA VIP 👑✨\n\n"
         "Escolha seu plano abaixo para gerar o Pix e garantir seu acesso VIP."
     )
     bot.send_message(message.chat.id, texto, reply_markup=markup)
 
 @bot.message_handler(commands=['id'])
 def pegar_id(message):
-    bot.send_message(message.chat.id, f"`ID deste chat: `{message.chat.id}`", parse_mode="Markdown")
+    bot.send_message(message.chat.id, f"`ID deste chat: {message.chat.id}`", parse_mode="Markdown")
 
 bot.infinity_polling()
